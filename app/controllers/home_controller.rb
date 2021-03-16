883155074517b763e1d69655345569eef84d89e8
Couragyn <couragyn@gmail.com>
2021-03-16 01:44:27 +0100

    Added JSON logic to controller. View pulls entry and updated date data from controller and displays it. Added padding to top as there is no nav

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,26 @@
 class HomeController < ApplicationController
   def index
+    require 'net/http'
+    require 'json'
+
+    highestCount = 0
+    highestEntry = 0
+
+    url = 'https://jon.endpoint.com/youtube-popular-20121222.json'
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    json = JSON.parse(response)
+
+    entries = json["feed"]["entry"]
+
+    entries.each_with_index do |entry, i|
+        if (thisEntryViews = entry["yt$statistics"]["viewCount"].to_i) > highestCount
+            highestCount = thisEntryViews
+            highestEntry = i
+        end 
+    end
+
+    @youtube_video = entries[highestEntry]
+    @last_updated = (json["feed"]["updated"]["$t"])[0...10]
   end
 end
